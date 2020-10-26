34c6955a98bbe6bf7cb8b55a135f4e6aab2f99d6
Bathory <skalolom@gmail.com>
2020-10-26 17:30:12 +0300

    в первом приближении реализована фильтрация и построение графиков

--- mainModule.py
+++ mainModule.py
@@ -8,6 +8,7 @@ from scipy import signal
 import os
 from scipy.fft import fft
 from scipy.signal import savgol_filter
+from scipy.ndimage import gaussian_filter1d
 dn = os.path.dirname(os.path.realpath(__file__))
 
 start = time.time()
@@ -23,8 +24,8 @@ pressVect = []
 names = []
 bars = []
 barColors = []
-pressMin = 5
-pressMax = 45
+pressMin = 10
+pressMax = 35
 # define r0
 r0 = 5
 rCurrent = r0
@@ -38,7 +39,8 @@ path = r'Data/5mm'
 logPath = r'Data/log.txt'
 logFile = open(logPath, 'a')
 # write type of experiment
-logFile.write(path.split(r'/')[-2] + '\n')
+experiment_name = path.split(r'/')[-1] + '\n'
+logFile.write(experiment_name)
 
 files = []
 # r=root, d=directories, f = files
@@ -96,19 +98,34 @@ for filename in files:
     # normalize timeVect to compensate radius deviation
     if filename.find('c_sh') != -1:
         rCurrent = 9.886 / 2
-        barColor = 'black'
+        if filename.find('Sp') != -1:
+            barColor = 'purple'
+        else:
+            barColor = 'black'
     if filename.find('c_sm') != -1:
         rCurrent = 9.875 / 2
-        barColor = 'magenta'
+        if filename.find('Sp') != -1:
+            barColor = 'purple'
+        else:
+            barColor = 'magenta'
     if filename.find('r_1') != -1:
         rCurrent = 10.039 / 2
-        barColor = 'blue'
+        if filename.find('Sp') != -1:
+            barColor = 'purple'
+        else:
+            barColor = 'blue'
     if filename.find('r_2') != -1:
         rCurrent = 10.347 / 2
-        barColor = 'green'
+        if filename.find('Sp') != -1:
+            barColor = 'purple'
+        else:
+            barColor = 'green'
     if filename.find('r_3') != -1:
         rCurrent = 10.260 / 2
-        barColor = 'red'
+        if filename.find('Sp') != -1:
+            barColor = 'purple'
+        else:
+            barColor = 'red'
 
     # if name of the experiment has changed, add whitespace between bars
     if radiusPrevious != rCurrent:
@@ -126,10 +143,11 @@ for filename in files:
 
     # making the low-pass filter
     # b, a = signal.butter(8, 0.01)
-    b, a = signal.butter(5, 0.001)
-    pressVect_flt = list(signal.filtfilt(b, a, pressVect))
-    pressVect_flt = savgol_filter(pressVect_flt,
-                                  2501, 5)
+    sos = signal.butter(5, 0.0005, output='sos')
+    #sos = signal.cheby1(4, 5, 0.01, output='sos')
+    pressVect_flt = signal.sosfiltfilt(sos, pressVect, padtype='constant')
+    # pressVect_flt = gaussian_filter1d(pressVect_flt, 4)
+    # pressVect_flt = savgol_filter(pressVect_flt, 101, 3)
     # save current values of pressureMax and pressureMin for calibration
     calibrationPressMax = max(pressVect_flt)
     calibrationPressMin = min(pressVect_flt)
@@ -158,15 +176,20 @@ for filename in files:
     bars.append(expTime)
     barColors.append(barColor)
     # names.append(filename.split(r'/')[-1][0:-4])
-    names.append(filename.split(r'/')[-1].split('.')[0].split('_')[2])
+    current_name = filename.split(r'/')[-1].split('.')[0].split('_')[2]
+    names.append(current_name)
     i += 1
     print('{0:.1f}% progress...'.format(100 * (i / len(files))), end=' ')
     # logFile.write(names[-1] + ' : ' + str(expTime) + '\n')
 
-    # # вычисляем спектр сигнала
+    # # # вычисляем спектр сигнала
     # plt.figure(i)
     # plt.title('Raw signal')
     # plt.plot(timeVect, pressVect, 'b', timeVect_flt, pressVect_flt, 'r')
+
+    # выводим на одном графике зависимости p(t)
+    plt.plot(timeVect_flt, pressVect_flt, barColor,
+             label=filename.split('/')[-1].split('.')[0])
 
 
 logFile.close()
@@ -174,20 +197,21 @@ logFile.close()
 font = {'family': 'normal', 'weight': 'bold', 'size': 20}
 
 plt.rcParams.update({'font.size': 22})
-plt.figure(1)
+# plt.figure(1)
+# plt.title(experiment_name)
 # plt.bar(names, bars, width=0.6)
 # define space between bars
-spaceBars = np.linspace(0, 0.6 * len(bars), len(bars))
-plt.bar(spaceBars, bars, width=0.6, color=barColors)
-plt.xticks(spaceBars, names)
-plt.tight_layout()
-plt.ylim([550, max(bars) + 10])
-
-i = 0
-for bar in bars:
-    if bar != 0:
-        plt.annotate(int(bar), xy=(spaceBars[i], bar + 1))
-    i += 1
+# spaceBars = np.linspace(0, 0.6 * len(bars), len(bars))
+# plt.bar(spaceBars, bars, width=0.6, color=barColors)
+# plt.xticks(spaceBars, names)
+# plt.tight_layout()
+# plt.ylim([200, max(bars) + 10])
+#
+# i = 0
+# for bar in bars:
+#     if bar != 0:
+#         plt.annotate(int(bar), xy=(spaceBars[i], bar + 1))
+#     i += 1
 
 end = time.time()
 print('evaluation time = {0:.2f} sec'.format(end - start))
@@ -197,6 +221,9 @@ print('evaluation time = {0:.2f} sec'.format(end - start))
 # plt.xlabel('time')
 # plt.ylabel('pressure')
 
+plt.title(experiment_name)
+plt.legend(loc='upper right')
+plt.grid()
 plt.show()
 
 """
